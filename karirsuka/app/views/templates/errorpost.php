a8630369069db35ac46c6956df9d7f27b960779e
dwilarasaati <71909756+dwilarasaati@users.noreply.github.com>
2021-07-10 14:36:34 +0800

    Create errorpost.php

---
+++ karirsuka/app/views/templates/errorpost.php
@@ -0,0 +1,16 @@
+<body>
+<div class="main">
+    <div class="card border-0 bg-body w-auto" style="margin:190px auto; border-radius: 30px;">
+        <div style="padding : 35px;" >
+            <div class="header-main" align="center">
+                <img src="<?= BASE_URL; ?>image/error.png" alt="Logo" style="width:140px;height:140px;margin-bottom:30px;">
+                <h3 class=primary>Error</h3>
+                <h5>Your Job vancacy was not submitted</h5>
+            </div>
+            <div class="d-grid gap-2 d-xl-flex justify-content-md-center" style="margin-top:50px;">
+                <a class="btn rounded-pill px-5 py-3 fw-bold fs-5" style ="background-color:#E21B1B; color:#FBFBFB" href="<?= BASE_URL; ?>perusahaan/postjob" role="button">Re-apply Now</a> 
+            </div>
+        </div> 
+    </div> 
+</div>  
+</body>
