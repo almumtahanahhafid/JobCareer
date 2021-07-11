7f20c780cdefb1190e4ccd27c406969d7d86e5f0
ennyfitriani <85067334+ennyfitriani@users.noreply.github.com>
2021-07-11 15:54:28 +0700

    Create successPost.php

---
+++ karirsuka/app/views/templates/successPost.php
@@ -0,0 +1,16 @@
+<body>
+<div class="main">
+    <div class="card border-0 bg-body w-auto" style="margin:190px auto; border-radius: 30px;">
+        <div style="padding : 35px;" >
+            <div class="header-main" align="center">
+                <img src="<?= BASE_URL; ?>image/checked.png" alt="Logo" style="width:140px;height:140px;margin-bottom:30px;">
+                <h3 class=primary>Successful</h3>
+                <h5>Your job vacancy was submitted</h5>
+            </div>
+            <div class="d-grid gap-2 d-xl-flex justify-content-md-center" style="margin-top:50px;">
+                <a class="btn rounded-pill px-5 py-3 fw-bold fs-5" style ="background-color:#8DAA9A; color:#FBFBFB" href="<?= BASE_URL; ?>perusahaan/history" role="button">See Job vacancy History</a> 
+            </div>
+        </div> 
+    </div> 
+</div>  
+</body>
